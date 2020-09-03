b13ae4cac2ad8f0e2cdb354c1ed2f337d9999dc3
Shubhi Jain <jainshubhi67@gmail.com>
2020-09-03 22:00:17 +0530

    Update SubarraySum.java

--- GeeksForGeeks/SubarraySum.java
+++ GeeksForGeeks/SubarraySum.java
@@ -3,8 +3,30 @@ public class SubarraySum {
     public static void main(String[] args) {
         int [] arr={3,5,7,4,9,3,10,18,1};
         int taregt=100;
-        int p1=0;
-        int p2=0;
+    }
+    private static void Approach1(int arr[],int target){
+        int len=arr.length;
+        for(int i=0;i<len;i++){
+            int currSum=arr[i];
+            for(int j=i+1;j<len;j++){
+                if(currSum==target){
+                    System.out.println(i+" "+j-1);
+                    return;
+                }
+                if(currSum>target){
+                    break;
+                }
+                currSum+=arr[j];
+            }
+            if(currSum==taregt){
+                System.out.println(i+" "+j);
+                return;
+            }
+        }
+        System.out.println("Subarray not found");
+    }
+    private static void Approach2(int arr[],int target){
+        int p1=0,p2=0;
         int curSum=0;
         while(p1!=arr.length){
             while(p2!=arr.length){
