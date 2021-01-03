da2acfb8695ce64ca93dc5d686da7aac238d250b
Shubhi Jain <shubhi.jain@altudo.co>
2021-01-03 18:14:23 +0530

    Last Index Of 1

--- GeeksForGeeks/LastIndexof1.java
+++ GeeksForGeeks/LastIndexof1.java
@@ -1,18 +1,41 @@
-public class LastIndexof1 {
-    public static void main(String[] args) {
-        int [] arr={3,5,7,4,9,3,10,18,1};
-        int size=arr.length;
-        FindLast(arr,size);
-    }
-    // TC : O(n) SC : O(1)
-    private static void FindLast(int arr[],int size){
-        int index=-1;
-        for(int i=size-1;i>=0;i--){
-            if(arr[i] == 1){
-                index=i;
-                break;
-            }
-        }
-       System.out.println(index);
-    }
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+		Scanner sc= new Scanner(System.in);
+		int testcases = sc.nextInt();
+		String str= sc.nextLine();
+		while(testcases-->0){
+		    str = sc.nextLine();
+		    System.out.println(GetLastIndex(str));
+		}
+	}
+	public static int GetLastIndex(String str){
+	    return GetLastIndex_Approach2(str);
+	}
+// 	Left to Right
+	public static int GetLastIndex_Approach1(String str){
+	    int index=-1;
+	    for(int i=0;i<str.length();i++){
+	        if(str.charAt(i)=='1'){
+	            index=i;
+	        }
+	    }
+	    return index;
+	}
+// 	Right to Left
+	public static int GetLastIndex_Approach2(String str){
+	    int index=-1;
+	    for(int i=str.length()-1;i>=0;i--){
+	        if(str.charAt(i)=='1'){
+	            return i;
+	        }
+	    }
+	    return index;
+	}
 }
